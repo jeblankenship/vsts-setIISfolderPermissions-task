9b1d90385574a8708b9dab6e911f9addc5b85d23
James Blankenship <jeblankenship@gmail.com>
2016-09-21 10:45:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rename Token Files
+Build task for VS Team Services that sets permission on IIS Web Application or web site sub folder.
+
+Initial Version: 09/21/2016
---
+++ task/set-IISfolderPermissions.ps1
@@ -0,0 +1,36 @@
+﻿[CmdletBinding()]
+param(
+    [string]$server,
+    [string]$webSiteName,
+    [string]$path,
+    [boolean]$anonymousAuthentication,
+    [boolean]$basicAuthentication,
+    [boolean]$windowsAuthentication
+)
+
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+Write-Verbose "Entering script $($MyInvocation.MyCommand.Name)"
+Write-Verbose "Parameter Values"
+$PSBoundParameters.Keys | % { Write-Verbose "  ${_} = $($PSBoundParameters[$_])" }
+
+$command = {  
+    param(
+        [string]$webSiteName,
+        [string]$path,
+        [boolean]$anonymousAuthentication,
+        [boolean]$basicAuthentication,
+        [boolean]$windowsAuthentication
+    )
+    Import-Module WebAdministration; 
+
+    $location = "$webSiteName/$path"
+    Write-Verbose "Setting authentication on $location"
+    Set-WebConfigurationProperty -filter /system.webServer/security/authentication/anonymousAuthentication -name enabled -value $anonymousAuthentication  -location $location;  
+    Set-WebConfigurationProperty -filter /system.webServer/security/authentication/basicAuthentication -name enabled -value $basicAuthentication -location $location;  
+    Set-WebConfigurationProperty -filter /system.webServer/security/authentication/windowsAuthentication -name enabled -value $windowsAuthentication -location $location; 
+    Write-Verbose "Authentication Set"
+}
+
+Invoke-Command -ComputerName $server $command -ArgumentList $webSiteName,$path,$anonymousAuthentication,$basicAuthentication,$windowsAuthentication
