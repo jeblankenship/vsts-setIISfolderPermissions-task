d185681bc547be2b1df2be16a2a5b1c4e6a5d22d
James Blankenship <jeblankenship@gmail.com>
2016-09-22 12:17:17 -0400

    Working, but need to figure out a few minor issues

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Rename Token Files
+# Set IIS Folder Permissions
 Build task for VS Team Services that sets permission on IIS Web Application or web site sub folder.
 
 Initial Version: 09/21/2016
--- task/set-IISfolderPermissions.ps1
+++ task/set-IISfolderPermissions.ps1
@@ -3,9 +3,9 @@ param(
     [string]$server,
     [string]$webSiteName,
     [string]$path,
-    [boolean]$anonymousAuthentication,
-    [boolean]$basicAuthentication,
-    [boolean]$windowsAuthentication
+    [string]$anonymousAuthentication,
+    [string]$basicAuthentication,
+    [string]$windowsAuthentication
 )
 
 Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
@@ -13,7 +13,7 @@ Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 
 Write-Verbose "Entering script $($MyInvocation.MyCommand.Name)"
 Write-Verbose "Parameter Values"
-$PSBoundParameters.Keys | % { Write-Verbose "  ${_} = $($PSBoundParameters[$_])" }
+$PSBoundParameters.Keys | % { Write-HOST "  ${_} = $($PSBoundParameters[$_])" }
 
 $command = {  
     param(
@@ -26,11 +26,14 @@ $command = {
     Import-Module WebAdministration; 
 
     $location = "$webSiteName/$path"
-    Write-Verbose "Setting authentication on $location"
+    Write-HOST "Setting authentication on $location"
     Set-WebConfigurationProperty -filter /system.webServer/security/authentication/anonymousAuthentication -name enabled -value $anonymousAuthentication  -location $location;  
     Set-WebConfigurationProperty -filter /system.webServer/security/authentication/basicAuthentication -name enabled -value $basicAuthentication -location $location;  
     Set-WebConfigurationProperty -filter /system.webServer/security/authentication/windowsAuthentication -name enabled -value $windowsAuthentication -location $location; 
     Write-Verbose "Authentication Set"
 }
+[bool]$anonymousAuthenticationBool= Convert-String $anonymousAuthentication Boolean
+[bool]$basicAuthenticationBool= Convert-String $basicAuthentication Boolean
+[bool]$windowsAuthenticationBool= Convert-String $windowsAuthentication Boolean
 
-Invoke-Command -ComputerName $server $command -ArgumentList $webSiteName,$path,$anonymousAuthentication,$basicAuthentication,$windowsAuthentication
+Invoke-Command -ComputerName $server $command -ArgumentList $webSiteName,$path,$anonymousAuthenticationBool,$basicAuthenticationBool,$windowsAuthenticationBool
